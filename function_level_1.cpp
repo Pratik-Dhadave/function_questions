4088201912a9a4f956abcef62bdfc797dc2b286a
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-21 11:55:53 +0530

    some multiple example on functions are done

--- function_level_1.cpp
+++ function_level_1.cpp
@@ -12,6 +12,47 @@ void msgPrint(){
     return;
     cout<<"message 2"<<endl;
 }
+
+void maxOfThree(int num1, int num2, int num3){
+  int ans=max(num1,num2);
+  int finalAns = max(ans,num3);
+  cout<<"maximum no is:"<<finalAns<<endl;
+}
+
+void countOf1toN(int n){
+    for(int i=1; i<=n; i++){
+      cout<<i<<endl;
+    }
+}
+int sumOf1toN(int n){
+  int sum=0;
+  for(int i=1; i<=n; i++){
+    sum+=i;
+  }
+  return sum;
+}
+void sumofEven1toN(int n){
+  int sum=0;
+  for(int i=2; i<=n; i=i+2){
+    sum+=i;
+  }
+  cout<<"Sum is:"<<sum<<endl;
+}
+void checkEvenOdd(int num){
+  if((num & 1)==0){
+    cout<<"Even No"<<endl;
+  }else{
+    cout<<"Odd No"<<endl;
+  }
+}
+bool checkPrime(int n){
+  for(int i=2; i<n; i++){
+    if(n%i==0){
+      return false;
+    }
+  }
+  return true;
+}
 int main()
 {
     // printMsg();
@@ -19,7 +60,25 @@ int main()
     // int sum =printSum(4,5,6);
     // cout<<sum<<endl;
 
-    msgPrint();
+    // msgPrint();
+
+    // maxOfThree(45,445,4);
+
+    // countOf1toN(10);
+
+    // int sum =sumOf1toN(10);
+    // cout<<"sum is :"<<sum<<endl;
+
+    // sumofEven1toN(10);
+
+    // checkEvenOdd(1);
+
+    bool prime =checkPrime(3);
+    if(prime){
+      cout<<"prime no"<<endl;
+    }else{
+      cout<<"not prime"<<endl;
+    }
     return 0;
 }
 
